c041ee56bf995b50abb2df78cb298e77bc771808
Bohdan Szymanik <bohdan.szymanik@gmail.com>
2025-05-14 09:43:19 +1200

    A little more secure

--- ClippingMeshblock2013ToTheCoastline.R
+++ ClippingMeshblock2013ToTheCoastline.R
@@ -3,8 +3,8 @@ library(dplyr)
 library(ggplot2)
 
 
-mb2013 <- st_read("C:\\Users\\user\\Downloads\\statsnz-meshblock-boundaries-2013-SHP\\meshblock-boundaries-2013.shp")
-nz_coastline <- st_transform(st_read("C:\\Users\\user\\Downloads\\lds-nz-coastline-mean-high-water-SHP\\nz-coastline-mean-high-water.shp"), st_crs(mb2013))
+mb2013 <- st_read("statsnz-meshblock-boundaries-2013-SHP\\meshblock-boundaries-2013.shp")
+nz_coastline <- st_transform(st_read("lds-nz-coastline-mean-high-water-SHP\\nz-coastline-mean-high-water.shp"), st_crs(mb2013))
 
 st_crs(mb2013) # EPSG:2193 = NZTM/NZCD2000
 st_crs(nz_coastline) # EPSG:2193 = NZTM/NZCD2000
