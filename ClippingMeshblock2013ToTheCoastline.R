ade594f1cca7baa9e479730c4682a5c79c4801c7
Bohdan Szymanik <bohdan.szymanik@gmail.com>
2025-05-14 10:46:46 +1200

    Now I can clip to cities with a circular boundary

--- ClippingMeshblock2013ToTheCoastline.R
+++ ClippingMeshblock2013ToTheCoastline.R
@@ -4,6 +4,47 @@ library(ggplot2)
 
 
 mb2013 <- st_read("statsnz-meshblock-boundaries-2013-SHP\\meshblock-boundaries-2013.shp")
+
+# first, let's chop up into 3 regions surrounding akld, wgtn, chch
+# Auckland is centred on -36.8402, 174.7791
+# Wellington is centred on -41.1941, 174.9282
+# Christchurch centred on -43.5110, 172.5995
+
+# I learn't here - you can't use st_transform to set crs when original crs is NA
+# you have to use st_set_crs
+akld_centre <- st_set_crs(st_as_sfc("POINT(174.7791 -36.8402 )"), "EPSG:4326")
+wgtn_centre <- st_set_crs(st_as_sfc("POINT(174.9282 -41.1941 )"), "EPSG:4326")
+chch_centre <- st_set_crs(st_as_sfc("POINT(172.5995 -43.5110 )"), "EPSG:4326")
+
+radius <- 50000
+
+akld_buffer <- st_buffer(akld_centre, dist=radius)
+wgtn_buffer <- st_buffer(wgtn_centre, dist=radius)
+chch_buffer <- st_buffer(chch_centre, dist=radius)
+
+mb2013_akld <- st_intersection(mb2013, st_transform(akld_buffer, "EPSG:2193"))
+mb2013_wgtn <- st_intersection(mb2013, st_transform(wgtn_buffer, "EPSG:2193"))
+mb2013_chch <- st_intersection(mb2013, st_transform(chch_buffer, "EPSG:2193"))
+
+ggplot(mb2013_akld) + geom_sf()
+ggplot(mb2013_wgtn) + geom_sf()
+ggplot(mb2013_chch) + geom_sf()
+
+akld_buffer <- st_buffer(akld_centre, dist=radius)
+wgtn_buffer <- st_buffer(wgtn_centre, dist=radius)
+chch_buffer <- st_buffer(chch_centre, dist=radius)
+
+mb2013_akld_clipped <- st_intersection(mb2013_clipped, akld_buffer)
+mb2013_wgtn_clipped <- st_intersection(mb2013_clipped, wgtn_buffer)
+mb2013_chch_clipped <- st_intersection(mb2013_clipped, chch_buffer)
+
+ggplot(mb2013_akld_clipped) + geom_sf()
+ggplot(mb2013_wgtn_clipped) + geom_sf()
+ggplot(mb2013_chch_clipped) + geom_sf()
+
+
+# now let's clip these to the coastline
+
 nz_coastline <- st_transform(st_read("lds-nz-coastline-mean-high-water-SHP\\nz-coastline-mean-high-water.shp"), st_crs(mb2013))
 
 st_crs(mb2013) # EPSG:2193 = NZTM/NZCD2000
@@ -20,51 +61,9 @@ ggplot(mb2013_clipped) + geom_sf()
 # no point in doing this part yet!
 st_write(mb2013_clipped, "mb2013_clipped.shp")
 
-# next part of my plan was to create circular clipped areas around cities
-# but I need to figure out why my original coastline clipping isn't working first!!!!
-
-
-# Auckland is centred on -36.84029281823505, 174.77910330635922
-# Wellington is centred on -41.19411697139204, 174.928276227328
-# Christchurch centred on -43.51103546580568, 172.59955702651035
-
-akld_centre <- st_point(c(174.77910330635922, -36.84029281823505 ))
-wgtn_centre <- st_point(c(174.928276227328, -41.19411697139204))
-chch_centre <- st_point(c(172.59955702651035, -43.51103546580568))
-
-sfc_akld_centre <- st_sfc(akld_centre)
-sfc_wgtn_centre <- st_sfc(wgtn_centre)
-sfc_chch_centre <- st_sfc(chch_centre)
-
-
-class(sfc_akld_centre)
-st_crs(sfc_akld_centre) # no coord system is yet assigned
-# either assign directly
-st_crs(sfc_akld_centre) = 2193
-
-# or from mb2013_clipped
-st_crs(mb2013_clipped) # 2193
-st_transform(sfc_akld_centre, st_crs(mb2013_clipped))
-
-akld_buffer <- st_buffer(sfc_akld_centre, dist=radius)
 
 
 
 
-radius <- 500000
 
-akld_buffer <- st_buffer(akld_centre, dist=radius)
-wgtn_buffer <- st_buffer(wgtn_centre, dist=radius)
-chch_buffer <- st_buffer(chch_centre, dist=radius)
-
-mb2013_akld_clipped <- st_intersection(mb2013_clipped, akld_buffer)
-mb2013_wgtn_clipped <- st_intersection(mb2013_clipped, wgtn_buffer)
-mb2013_chch_clipped <- st_intersection(mb2013_clipped, chch_buffer)
-
-ggplot(mb2013_akld_clipped) + geom_sf()
-ggplot(mb2013_wgtn_clipped) + geom_sf()
-ggplot(mb2013_chch_clipped) + geom_sf()
-
-
-
-
+mb2013_akld <- st_intersection(mb2013, akld_buffer)
